10716271d765e3b4388129be19e08b929021ebbb
Eric Silk <eric.silk@ericsilk.com>
2023-03-21 17:11:41 -0500

    fixed, optimal step size for GD, typehinting, little bit of cleanup

--- lab2_sp2023_files/newtons.py
+++ lab2_sp2023_files/newtons.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+from typing import Tuple
+import multiprocessing as mp
 import numpy as np
 import matplotlib.pyplot as plt
-import multiprocessing as mp
 
 
 class F:
     def __init__(self) -> None:
         pass
 
-    def __call__(self, z) -> np.ndarray:
+    def __call__(self, z: np.ndarray) -> np.ndarray:
         assert z.ndim == 1
         assert z.shape[0] == 2
 
@@ -15,7 +17,7 @@ class F:
 
         return x**3 * y - x * y**3 - y
 
-    def grad(self, z) -> np.ndarray:
+    def grad(self, z: np.ndarray) -> np.ndarray:
         assert z.ndim == 1
         assert z.shape[0] == 2
         x, y = z[0], z[1]
@@ -23,7 +25,7 @@ class F:
         grad = np.array([3 * x**2 * y - y**3, x**3 - 3 * x * y**2 - 1])
         return grad
 
-    def hessian(self, z) -> np.ndarray:
+    def hessian(self, z: np.ndarray) -> np.ndarray:
         assert z.ndim == 1
         assert z.shape[0] == 2
         x, y = z[0], z[1]
@@ -33,23 +35,32 @@ class F:
         hess = np.array([[a, b], [b, -a]])
         return hess
 
-    def hessian_inv(self, z) -> np.ndarray:
+    def hessian_inv(self, z: np.ndarray) -> np.ndarray:
         return np.linalg.pinv(self.hessian(z))
 
 
-def newton_method(z0: np.array, N=50) -> np.array:
+def newton_method(z0: np.array, N: int = 50) -> np.ndarray:
     # Write your code here.
     # If needed, you can define other functions as well to be used here.
     # Input z0 and output zN should be numpy.ndarray objects with 2 elements:
     # e.g. np.array([ x , y ]).
     f = F()
     zN = z0
+    # TODO add convergence check?
     for i in range(N):
+        if not np.any(zN):
+            # Per Piazza, stop iterating if you land at the origin
+            return zN
         zN -= f.hessian_inv(zN) @ f.grad(zN)
+
     return zN
 
 
-def plot_image(s_points: np.array, n=500, domain=(-1, 1, -1, 1)):
+def plot_image(
+    s_points: np.array,
+    n: int = 500,
+    domain: Tuple[float, float, float, float] = (-1.0, 1.0, -1.0, 1.0),
+):
     m = np.zeros((n, n))
     xmin, xmax, ymin, ymax = domain
     for ix, x in enumerate(np.linspace(xmin, xmax, n)):
@@ -58,13 +69,14 @@ def plot_image(s_points: np.array, n=500, domain=(-1, 1, -1, 1)):
             zN = newton_method(z0)
             code = np.argmin(np.linalg.norm(s_points - zN, ord=2, axis=1))
             m[iy, ix] = code
+
     plt.imshow(m, cmap="brg")
     plt.axis("off ")
     plt.savefig("q2_hw3.png ")
     plt.show()
 
 
-def job(job_spec):
+def job(job_spec: Tuple[np.ndarray, int, float, int, float]) -> Tuple[int, int, int]:
     s_points, ix, x, iy, y = job_spec
     z0 = np.array([x, y])
     zN = newton_method(z0)
@@ -74,11 +86,14 @@ def job(job_spec):
     return rslt
 
 
-def plot_image_parallel(s_points: np.array, n=500, domain=(-1, 1, -1, 1)):
+def plot_image_parallel(
+    s_points: np.ndarray,
+    n: int = 500,
+    domain: Tuple[float, float, float, float] = (-1.0, 1.0, -1.0, 1.0),
+):
     m = np.zeros((n, n))
     xmin, xmax, ymin, ymax = domain
     job_specs = []
-    # TODO cartesian product
     for ix, x in enumerate(np.linspace(xmin, xmax, n)):
         for iy, y in enumerate(np.linspace(ymin, ymax, n)):
             job_specs.append((s_points, ix, x, iy, y))
@@ -91,6 +106,8 @@ def plot_image_parallel(s_points: np.array, n=500, domain=(-1, 1, -1, 1)):
         m[iy, ix] = code
 
     plt.imshow(m, cmap="brg")
+    for p in s_points:
+        plt.scatter(p[0], p[1])
     plt.axis("off")
     # plt.savefig("q2_hw3.png")
     plt.show()
@@ -100,8 +117,16 @@ if __name__ == "__main__":
     # Example of usage .
     # In this example , the stationary points are (0 , 0 ) , (1 , 1 ) , (2 , 2 ) and (3 , 3 ) .
     # Replace these with the ones obatined in part a).
+
+    # Calculated using WolframAlpha
     stationary_points = np.array(
-        [[-1.0 / 2.0, np.sqrt(3) / 2.0], [-1.0 / 2.0, -np.sqrt(3) / 2.0], [1.0, 0.0]]
+        [
+            [-1.0 / 2.0, np.sqrt(3.0) / 2.0],
+            [-1.0 / 2.0, -np.sqrt(3.0) / 2.0],
+            [1.0, 0.0],
+        ]
     )
+
+    # Old serial implementation, de-comment to enable
     # plot_image(stationary_points)
     plot_image_parallel(stationary_points, n=500)
--- lab2_sp2023_files/projected_gd.py
+++ lab2_sp2023_files/projected_gd.py
@@ -1,27 +1,36 @@
+#!/usr/bin/env python3
+from typing import Callable
 import asgn_source as asou
 import numpy as np
 import scipy.optimize as so
 
 
+DIM = 25
 MAX_ITER = 1000
 EPSILON = 0.01
 SEED = 1234
 
 
 class F:
-    def __init__(self, Q, b, c):
+    def __init__(self, Q: np.ndarray, b: np.ndarray, c: float):
         self.Q = Q
         self.b = b
         self.c = c
 
-    def __call__(self, x):
+    def __call__(self, x: np.ndarray) -> float:
         return asou.opfun(x, self.Q, self.b, self.c)
 
-    def grad(self, x):
+    def grad(self, x: np.ndarray) -> np.ndarray:
         return x.T @ self.Q + self.b
 
 
-def armijo(x, f, t0=1.0, alpha=0.1, beta=0.5):
+def armijo(
+    x: np.ndarray,
+    f: F,
+    t0: float = 1.0,
+    alpha: float = 0.1,
+    beta: float = 0.5,
+) -> float:
     t = t0
     grad_x = f.grad(x)
     delta_x = -grad_x
@@ -36,12 +45,31 @@ def armijo(x, f, t0=1.0, alpha=0.1, beta=0.5):
     return t
 
 
-def grad_descent(x, f):
-    alpha = armijo(x, f)
+def calculate_optimal_alpha(Q: np.ndarray, scaling: float = 0.99) -> float:
+    """
+    Given a QuadraticForm problem, determine the optimal stepsize alpha.
+    args:
+        f: a QuadraticForm problem
+        scaling: We need alpha < 2/L, so calculate 2/L and scale it by this value (default=0.99)
+    """
+    s = np.linalg.svd(Q, compute_uv=False)
+    L = np.max(s).item()
+    # Make it just slightly less than 2/L
+    alpha = (2 / L) * scaling
+    print("Optimal alpha:", alpha)
+
+    return alpha
+
+
+# TODO fix this, the prompt asks for a fixed step size
+def grad_descent(x: np.ndarray, f: F, alpha: float) -> np.ndarray:
+    # alpha = armijo(x, f)
     return x - alpha * f.grad(x)
 
 
-def project_coordinate(x, constraint_min, constraint_max, n):
+def project_coordinate(
+    x: np.ndarray, constraint_min: np.ndarray, constraint_max: np.ndarray, n: int
+) -> np.ndarray:
     ### write your code here
     assert x.ndim == 1
     assert x.shape[0] == n
@@ -52,15 +80,22 @@ def project_coordinate(x, constraint_min, constraint_max, n):
     return x_projected
 
 
-def run_projected_GD(constraint_min, constraint_max, Q, b, c, n):
+def run_projected_GD(
+    constraint_min: np.ndarray,
+    constraint_max: np.ndarray,
+    Q: np.ndarray,
+    b: np.ndarray,
+    c: float,
+    n: int,
+) -> np.ndarray:
     ### write your code here
-    result = None
     f = F(Q, b, c)
 
     x0 = np.random.rand(n)
     x_i = x0
+    alpha = calculate_optimal_alpha(Q)
     for i in range(MAX_ITER):
-        x_next = grad_descent(x_i, f)
+        x_next = grad_descent(x_i, f, alpha)
         x_next = project_coordinate(x_next, constraint_min, constraint_max, n)
         diff = np.linalg.norm(x_next - x_i, ord=2)
         if diff <= EPSILON:
@@ -71,14 +106,16 @@ def run_projected_GD(constraint_min, constraint_max, Q, b, c, n):
         raise RuntimeError(f"Failed to converge in {MAX_ITER} iterations!")
 
 
-def check_solution(constraint_min, constraint_max, f, n):
+def check_solution(
+    constraint_min: np.ndarray, constraint_max: np.ndarray, f: F, n: int
+) -> np.ndarray:
     bounds = so.LinearConstraint(np.eye(n), lb=constraint_min, ub=constraint_max)
     result = so.minimize(f, np.random.rand(n), constraints=(bounds,))
     return result
 
 
 if __name__ == "__main__":
-    n = 25
+    n = DIM
     np.random.seed(SEED)
     constraint_min, constraint_max, Q_val, b_val, c_val = asou.get_parameters(n)
     armijo_sol = run_projected_GD(
